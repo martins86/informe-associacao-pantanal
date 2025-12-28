528459cc4f73f9090c9891e65d1a3c90dcdb8855
Thiago Martins de Freitas <46196327+martins86@users.noreply.github.com>
2025-12-28 00:29:56 +0000

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule, MatMenuModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {}
+export class App { }
